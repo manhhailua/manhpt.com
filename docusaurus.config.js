dffa3cd3d3383024e5b9a7d17b339d6dac3f9269
Manh Pham <v.manhpt1@vinbigdata.org>
2022-05-07 11:31:55 +0700

    fix: update locale and global info

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,15 +16,15 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "manhhailua", // Usually your GitHub org/user name.
+  projectName: "manhpt.com", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: "vi",
+    locales: ["vi"],
   },
 
   presets: [
