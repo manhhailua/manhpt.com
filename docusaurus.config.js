54b4095d48bda8ab0d066bdc91a359929ce26582
Mạnh Phạm <manhhailua@gmail.com>
2026-03-15 21:38:14 +0700

    feat: add custom Open Graph image for blog
    - Create custom OG image: manhpt-og-custom-red.jpg (1200x630)
    - Red accent (#E53935) on dark blue background (#1A1A2E)
    - Tech patterns: hexagon grid, circuit lines
    - Update docusaurus.config.js to use new image
    - Improve social media previews

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -90,7 +90,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/manhpt-og-custom-red.jpg",
       navbar: {
         title: "ManhPT",
         logo: {
